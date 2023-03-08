e31bb55280e92bdfe2b7a78f80237c3a13381ad5
SirIsaacNeutron <dbadir@cox.net>
2023-03-08 02:02:48 -0800

    Add API calls

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "./styles.css"
+
+const weatherInput = document.querySelector(".weather-form input")
+
+const weatherForm = document.querySelector(".weather-form")
+
+weatherForm.addEventListener("submit", e => {
+    e.preventDefault()
+})
+
+weatherInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        const cityName = weatherForm.querySelector("input").value
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=51508559a54a70928ab3aed23d3a0b63`, {
+            mode: "cors",
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+
+            const latitude = json.coord.lat
+            const longitude = json.coord.lon
+
+            getWeatherInfo(latitude, longitude)
+
+        })
+
+    }
+})
+
+function getWeatherInfo(latitude, longitude) {
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=51508559a54a70928ab3aed23d3a0b63`,
+    {
+        mode: "cors"
+    })
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,13 @@ module.exports = {
             filename: "index.html",
             template: "src/template.html",
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    }
 }
