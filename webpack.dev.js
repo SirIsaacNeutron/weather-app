37fd6994e48cbcd1f7eddc6005b0db6da8668a67
SirIsaacNeutron <dbadir@cox.net>
2023-03-15 16:14:38 -0700

    Add separate config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,14 +2,14 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "[name][hash].js",
+        filename: "[name][contenthash].js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
-    // devtool: "inline-source-map",
+    devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
             title: "Weather",
---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "[name][contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    // devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Weather",
+            filename: "index.html",
+            template: "src/template.html",
+            favicon: "src/storm.png"
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    }
+}
